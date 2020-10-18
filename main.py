356cbb616acd501e75f5bb6c2079d6594d395a2d
lahl <domee.lahl@gmail.com>
2020-10-18 20:07:42 +0200

    Prototype code structure

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 NUMBER_COMPARED = 0
 NUMBER_SWITCHED = 0
 
+
+def find_smallest_number():
+    pass
+
+
+def iteration():
+    pass
+
+
+def selection_sort():
+    pass
+
+
 if __name__ == '__main__':
     ba = [1, 4, 7, 8, 11, 3, 15, 16, 17]
     bb = [22, 25, 45, 3, 1, 8, 15, 19, 22]
