aebfa71999fde60a1c874f87690a2bc38f124abc
Tesselay Avari <domee.lahl@gmail.com>
2020-10-21 00:06:27 +0200

    Fix selection sort again

--- main.py
+++ main.py
@@ -6,14 +6,12 @@ def find_smallest_index(numbers):
     global COUNT_COMPARED
 
     smallest_value = numbers[0]
-    index = 0
-    for i in range(1, len(numbers) - 1):
+    for i in range(1, len(numbers)):
         if numbers[i] < smallest_value:
-            index = i
-            break
-        index = 0
-
+            smallest_value = numbers[i]
         COUNT_COMPARED += 1
+
+    index = numbers.index(smallest_value)
 
     return index
 
@@ -38,12 +36,3 @@ def selection_sort(numbers):
     COUNT_MOVED = 0
 
     return sorted_numbers
-
-
-if __name__ == '__main__':
-    ba = [1, 4, 7, 8, 11, 3, 15, 16, 17]
-    bb = [22, 25, 45, 3, 1, 8, 15, 19, 22]
-    bc = [99, 74, 55, 12, 65, 98, 8, 11, 23]
-    bd = [9, 8, 7, 6, 5, 4, 3, 2, 1]
-
-    print(selection_sort(ba))
